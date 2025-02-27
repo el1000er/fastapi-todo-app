d31e4792ed9fcc303b19315e0af2f81ecc1ac51a
Your Name <you@example.com>
2025-02-27 14:53:52 -0300

    Modified database connection to use sqlite todosapp.db

--- project_to_prod/database.py
+++ project_to_prod/database.py
@@ -3,11 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 ## SQL lite connection
-# SQLACHEMY_DATABASE_URL =  'sqlite:///./todosapp.db'
-# engine = create_engine(SQLACHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
+SQLACHEMY_DATABASE_URL =  'sqlite:///./project_to_prod/todosapp.db'
+engine = create_engine(SQLACHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
 
-SQLACHEMY_DATABASE_URL =  'postgresql://postgres:asdzxc3033@localhost/TodoApplicationDatabase'
-engine = create_engine(SQLACHEMY_DATABASE_URL )
+# SQLACHEMY_DATABASE_URL =  'postgresql://postgres:asdzxc3033@localhost/TodoApplicationDatabase'
+# engine = create_engine(SQLACHEMY_DATABASE_URL )
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
